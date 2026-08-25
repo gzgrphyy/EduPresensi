4c1ddda97dc11a46672d818c2874f7eac2b4fe3b
gzgrphyy <gzgrphy@gmail.com>
2026-08-25 18:28:20 +0700

    fix: bahasa english pada murid alpha

--- app/pages/admin/pengaturan/index.vue
+++ app/pages/admin/pengaturan/index.vue
@@ -396,11 +396,11 @@ async function handleSave() {
                 </div>
 
                 <!-- Favicon -->
-                <div class="bg-white dark:bg-gray-800 rounded-lg border admin-accent-border p-">
+                <div class="bg-white dark:bg-gray-800 rounded-lg border admin-accent-border p-4">
                   <label class="block text-sm  text-gray-700 dark:text-gray-300 mb-3">
                     <span class="flex items-center gap-1.5">
-                      <svg class="w-7 h-7 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
+                      <svg class="w-4 h-4 text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                       </svg>
                       {{ t('admin.pengaturan.labelFavicon') }}
                     </span>
